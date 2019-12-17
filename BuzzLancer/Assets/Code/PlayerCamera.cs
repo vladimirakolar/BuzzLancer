1d874438f7a60010ab425908b9dddcd8f3053ad7
Vladimira <vladimirakolar@gmail.com>
2019-12-17 19:00:10 +0100

    Added private metod.

--- BuzzLancer/Assets/Code/PlayerCamera.cs
+++ BuzzLancer/Assets/Code/PlayerCamera.cs
@@ -1,14 +1,17 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
     
 namespace Assets.Code
 { 
 
     public class PlayerCamera
     {
+        private readonly Player _player;
+        private readonly Camera _camera;
+
         public PlayerCamera(Player player, Camera camera)
         {
-
+            _player - player;
+            _camera - camera;
         }
 
     }
