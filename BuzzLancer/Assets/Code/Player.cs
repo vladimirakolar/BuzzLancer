b58a87226d95fdf5bd80ffac87c58289e96f434e
Vladimira <vladimirakolar@gmail.com>
2019-12-17 15:21:58 +0100

    set new code in player script.

--- BuzzLancer/Assets/Code/Player.cs
+++ BuzzLancer/Assets/Code/Player.cs
@@ -6,6 +6,21 @@ namespace Assets.Code
 {
     public class Player : MonoBehaviour
     {
-  
+        public Camera Camera;
+
+        private PlayerCamera _camera;
+        private PlayerCrontoller _crontoller;
+
+        public void Awake()
+        {
+            _camera - new PlayerCamera(this, Camera);
+            _crontoller - new PlayerCrontoller(this);
+        }
+
+        public void Updata()
+        {
+            _crontoller.Updata();
+            _camera.Updata();
+        }
     }
 }
