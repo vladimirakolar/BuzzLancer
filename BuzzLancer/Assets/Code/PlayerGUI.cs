43c49671e527956eee1b5dcd586fbd2c5f884298
Vladimira <vladimirakolar@gmail.com>
2019-12-18 17:52:09 +0100

    Set new color to becground and forground.

--- BuzzLancer/Assets/Code/PlayerGUI.cs
+++ BuzzLancer/Assets/Code/PlayerGUI.cs
@@ -4,6 +4,24 @@ namespace Assets.Code
 {
     public class PlayerGUI
     {
+        private readonly PlayerController _controller;
+        private readonly Player _player;
 
+        private readonly ProgresBar _velocityProgresBar;
+
+
+        public PlayerGUI(PlayerController controller, Player player)
+        {
+            _controller = controller;
+            _player = player;
+
+            _velocityProgresBar = new ProgresBar
+            {
+                Size = new Vector2(250, 10),
+                Position = new Vector2(10, Screen.height - 10 - 10),
+                BeckgroundColor=new Color(199,231,255),
+                ForegroundColor=new Color(0,145,255)
+            };
+        }
     }
 }
