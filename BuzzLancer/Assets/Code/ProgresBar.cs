6657ded1da9e0e284aad761e7c0e0bab34f0e8ef
Vladimira <vladimirakolar@gmail.com>
2019-12-18 17:13:15 +0100

    Written new code for Progress Bar class.

--- BuzzLancer/Assets/Code/ProgresBar.cs
+++ BuzzLancer/Assets/Code/ProgresBar.cs
@@ -1,7 +1,44 @@
 ﻿using UnityEngine;
-using UnityEditor;
 
-public class ProgresBar 
+namespace Assets.Code
 {
- 
+    public class ProgresBar
+    {
+        private readonly GameObject _gameObject;
+
+        private float _value;
+
+        public Color BeckgroundColor { get; set; }
+
+        public Color ForegroundColor { get; set; }
+
+        public float MaxValue { get; set; }
+
+        public Vector2 Position { get; set; }
+
+        public Vector2 Size { get; set; }
+
+        public bool IsEnabled { get { return _gameObject.activeSelf; } set { _gameObject.SetActive(value); } }
+
+        public float Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = Mathf.Clamp(value, 0, MaxValue);
+            }
+        }
+
+        public ProgresBar()
+        {
+            _gameObject = new GameObject();
+            _gameObject.AddComponent<ProgresBarRenderer>().Init(this);
+        }
+
+        public void Destroy()
+        {
+            Object.Destroy(_gameObject);
+
+        }
+    }
 }
