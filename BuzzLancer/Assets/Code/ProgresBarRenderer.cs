104b1cb5ca4c747014ecbbfdf5a3f15d454ccbd4
Vladimira <vladimirakolar@gmail.com>
2019-12-18 17:27:19 +0100

    Written new method in Progres Bar Renderer.

--- BuzzLancer/Assets/Code/ProgresBarRenderer.cs
+++ BuzzLancer/Assets/Code/ProgresBarRenderer.cs
@@ -5,6 +5,24 @@ namespace Assets.Code
 {
     public class ProgresBarRenderer : MonoBehaviour
     {
+        private ProgresBar _bar;
 
+        public void Init(ProgresBar bar)
+        {
+            _bar = bar;
+        }
+
+        public void OnGUI()
+        {
+            var oldColor = GUI.color;
+
+            GUI.color = _bar.BeckgroundColor;
+            GUI.DrawTexture(new Rect(_bar.Position.x, _bar.Position.y, _bar.Size.x, _bar.Size.y),GameResorces.Square);
+
+            GUI.color = _bar.ForegroundColor;
+            GUI.DrawTexture(new Rect(_bar.Position.x, _bar.Position.y, _bar.Value*_bar.Size.x/_bar.MaxValue, _bar.Size.y), GameResorces.Square);
+
+            GUI.color = oldColor;
+        }
     }
 }
